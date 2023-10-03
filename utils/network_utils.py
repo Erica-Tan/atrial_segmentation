5737079a30a1d1f2eb1be412405d0cd2a4f84f4d
Erica <erica.itk@gmail.com>
2023-10-04 12:01:17 +1300

    Update network_utils.py

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -119,49 +119,19 @@ def get_model(args):
 if __name__ == "__main__":
     import torch
     from ptflops import get_model_complexity_info
-    # from networks.dim2.attention_unet import AttentionUnet
-    #
-    # model = AttentionUnet(
-    #     spatial_dims=2,
-    #     in_channels=1,
-    #     out_channels=4,
-    #     channels=(32, 64, 128, 256, 512),
-    #     strides=(2, 2, 2, 2),
-    #     kernel_size= 5,
-    #     up_kernel_size= 5,
-    #
-    #     dropout=0.2,
-    # )
-
-    # from monai.networks.nets import UNet
-    #
-    # model = UNet(
-    #     spatial_dims=2,
-    #     in_channels=1,
-    #     out_channels=4,
-    #     channels=(32, 64, 128, 256, 512),
-    #     strides=(2, 2, 2, 2),
-    #     num_res_units=3,
-    #     kernel_size=5,
-    #     up_kernel_size=5,
-    #     norm='instance',
-    #     dropout=0.2
-    # )
-
-    #
-    # from networks.dim2.basic_unetplusplus import BasicUNetPlusPlus
-    #
-    # model = BasicUNetPlusPlus(
-    #         spatial_dims=2,
-    #         in_channels=1,
-    #         out_channels=3,
-    #         features=(32, 64, 128, 256, 512, 32),
-    #         norm='instance',
-    #         dropout=0.2,
-    #         deep_supervision=True,
-    #         act='prelu',
-    #         kernel_size=5
-    #     )
+    from networks.dim2.basic_unetplusplus import BasicUNetPlusPlus
+    
+    model = BasicUNetPlusPlus(
+             spatial_dims=2,
+             in_channels=1,
+             out_channels=3,
+             features=(32, 64, 128, 256, 512, 32),
+             norm='instance',
+             dropout=0.2,
+             deep_supervision=True,
+             act='prelu',
+             kernel_size=5
+         )
 
     # print(model)
 
@@ -169,67 +139,4 @@ if __name__ == "__main__":
     # print(x.shape, x.dtype)
     # output = model(x)
     # # print(output[0].shape)
-
-    # from monai.networks.nets import SwinUNETR
-    #
-    # model =  SwinUNETR(
-    #     img_size=(32, 96, 96),
-    #     in_channels=1,
-    #     out_channels=4,
-    #     feature_size=48,
-    #     drop_rate=0,
-    #     attn_drop_rate=0.0,
-    #     dropout_path_rate=0.0,
-    #     use_checkpoint=True,
-    # )
-
-    from torch import nn
-    from dynamic_network_architectures.architectures.unet import PlainConvUNet
-    from scripts.nnUnet.utils import get_matching_instancenorm, convert_dim_to_conv_op
-
-    # 3d network
-    conv_kernel_sizes = [[1, 3, 3], [1, 3, 3], [3, 3, 3], [3, 3, 3], [3, 3, 3], [3, 3, 3], [3, 3, 3]]
-    num_stages = len(conv_kernel_sizes)
-    UNet_base_num_features = 32
-    unet_max_num_features = 320
-    features_per_stage = [min(UNet_base_num_features * 2 ** i,
-                              unet_max_num_features) for i in range(num_stages)]
-    dim = len(conv_kernel_sizes[0])
-    conv_op = convert_dim_to_conv_op(dim)
-    pool_op_kernel_sizes = [[1, 1, 1], [1, 2, 2], [1, 2, 2], [2, 2, 2], [2, 2, 2], [1, 2, 2], [1, 2, 2]]
-    n_conv_per_stage_encoder = [2, 2, 2, 2, 2, 2, 2]
-    n_conv_per_stage_decoder = [2, 2, 2, 2, 2, 2]
-    conv_or_blocks_per_stage = {
-        'n_conv_per_stage': n_conv_per_stage_encoder,
-        'n_conv_per_stage_decoder': n_conv_per_stage_decoder
-    }
-    kwargs = {
-        'conv_bias': True,
-        'norm_op': get_matching_instancenorm(conv_op),
-        'norm_op_kwargs': {'eps': 1e-5, 'affine': True},
-        'dropout_op': None,
-        'dropout_op_kwargs': None,
-        'nonlin': nn.LeakyReLU,
-        'nonlin_kwargs': {'inplace': True},
-    }
-
-    model = PlainConvUNet(
-        input_channels=1,
-        n_stages=num_stages,
-        features_per_stage=features_per_stage,
-        conv_op=conv_op,
-        kernel_sizes=conv_kernel_sizes,
-        strides=pool_op_kernel_sizes,
-        num_classes=4,
-        deep_supervision=False,
-        **conv_or_blocks_per_stage,
-        **kwargs
-    )
-
-    macs, params = get_model_complexity_info(model, (1, 20, 320, 256), as_strings=True,
-                                             print_per_layer_stat=False, verbose=False)
-    print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
-    print('{:<30}  {:<8}'.format('Number of parameters: ', params))
-
-
 
